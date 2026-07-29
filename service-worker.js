5d69739a250a313280b32db8acb14030b5aab076
aldoddjr-cyber <aldoddjr@gmail.com>
2026-07-28 19:16:30 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+var OMADA_CACHE_NAME = 'omada-deck-v1';
+var ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(OMADA_CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) { return key !== OMADA_CACHE_NAME; })
+            .map(function (key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request);
+    })
+  );
+});
